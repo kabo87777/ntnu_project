58f7aa08e9fbb4ae475d3536748023a6a8dbaae7
kabo87777 <57362133+kabo87777@users.noreply.github.com>
2021-11-07 21:04:48 +0800

    Update test.py
    crawler

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print('hello')
+import requests
+import pandas as pd
+
+dates = [20211101]
+stockNo = 2330
+url_template = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date={}&stockNo={}"
+
+for date in dates :
+    url = url_template.format(date, stockNo)
+    file_name = "{}_{}.csv".format(stockNo, date)
+    
+    data = pd.read_html(requests.get(url).text)[0]
+    data.columns = data.columns.droplevel(0)
+    data.to_csv(file_name, index=False,encoding="utf-8-sig")
