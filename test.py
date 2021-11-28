485bc4d2898361ffe47ba42dc9af479d7c69bf1e
kabo87777 <57362133+kabo87777@users.noreply.github.com>
2021-11-28 17:43:05 +0800

    draw

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-import requests
-import pandas as pd
+import pandas_datareader as pdr
+import mplfinance as mpf
+import datetime as datetime
 
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+start = datetime.datetime(2021,9,27)
+df_2330 = pdr.DataReader('2330.TW', 'yahoo', start=start)
+print(df_2330)
+mpf.plot(df_2330,type='candle', mav=(5,10,20),volume=True)
+=======
+>>>>>>> Stashed changes
 dates = [20211001,20211101]
 stockNo = 2330
 url_template = "https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=html&date={}&stockNo={}"
@@ -12,3 +22,4 @@ for date in dates :
     data = pd.read_html(requests.get(url).text)[0]
     data.columns = data.columns.droplevel(0)
     data.to_csv(file_name, index=False,encoding="utf-8-sig")
+>>>>>>> 69ba26dbf8dccf86434e8d4e6b6d7e4923138e4b
