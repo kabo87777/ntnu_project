075e8b988599e1c32c9144e3d83bced18c882d12
kabo87777 <57362133+kabo87777@users.noreply.github.com>
2021-11-28 20:39:31 +0800

    RSI

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 import mplfinance as mpf
 import pandas_datareader as pdr
 import datetime as datetime
+from talib import abstract
+
+def RSI(df, period):
+    return abstract.RSI(df, timeperiod=period)
+
+stock_num = str(input("enter stock number:"))
+
 start = datetime.datetime(2021,9,28)
-df_2330 = pdr.DataReader('2330.TW', 'yahoo', start=start)
-df_2330.index.name = 'Date'
-mpf.plot(df_2330,type='candle', mav=(5,10,20),volume=True)
+stock_df = pdr.DataReader(stock_num+'.TW', 'yahoo', start=start)
+if(stock_df is not None):
+    stock_df["close"] = stock_df["Close"]
+    mc = mpf.make_marketcolors(up='r',down='g',volume='in')
+    s  = mpf.make_mpf_style(marketcolors=mc)
+    index  = mpf.make_addplot(RSI(stock_df, 14), panel = 2, ylabel = 'RSI', color = 'lime')
+    mpf.plot(stock_df,type='candle', style = s, mav=(5,10,20), addplot = [index],volume=True)
+else:
+    print("wrong number")
