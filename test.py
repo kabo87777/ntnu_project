792c010a4ee2f18ff00c1e20fe22d77622931cbf
chopper10251 <kartd0099781@gmail.com>
2021-12-07 20:40:35 +0800

    Update test.py
    add comments
    
    change the color

--- test.py
+++ test.py
@@ -16,18 +16,33 @@ stock_df = pdr.DataReader(stock_num+'.TW', 'yahoo', start=start)
 if(stock_df is not None):
     stock_df["close"] = stock_df["Close"]
     close = [float(x) for x in stock_df["close"]]
-    stock_df["EMA12"] = talib.EMA(np.array(close),timeperiod=6)
-    stock_df["EMA26"] = talib.EMA(np.array(close),timeperiod=12)
+
+    ###指數平均線
+    # stock_df["EMA12"] = talib.EMA(np.array(close),timeperiod=6)
+    # stock_df["EMA26"] = talib.EMA(np.array(close),timeperiod=12)
+
+    ###MACD
     stock_df["MACD"],stock_df["MACDsignal"],stock_df["MACDhist"] = talib.MACD(np.array(close),fastperiod = 6,slowperiod = 12,signalperiod = 9)
+    
+    ###KD值
     stock_df['k'], stock_df['d'] = talib.STOCH(stock_df['High'], stock_df['Low'], stock_df['close'],fastk_period = 9)
-    #print(stock_df)
-    mc = mpf.make_marketcolors(up='r',down='g',volume='in')
-    s  = mpf.make_mpf_style(marketcolors=mc)
+    
+    ##print(stock_df)
+
+    mc = mpf.make_marketcolors(up='r',down='g', edge='', wick='inherit',volume='inherit')
+
+    s = mpf.make_mpf_style(gridaxis='both',gridstyle='-.',y_on_right=True,marketcolors=mc,edgecolor='black',figcolor='white',facecolor='black', gridcolor='gray')
+    ##reference :https://blog.csdn.net/weixin_48964486/article/details/116229333
+
+
+
     index  = [mpf.make_addplot(stock_df["MACD"], panel = 2,type='bar', ylabel = 'MACD', color = 'red'),
               mpf.make_addplot(RSI(stock_df, 14), panel = 3, ylabel = 'RSI', color = 'lime'),
               mpf.make_addplot(stock_df["k"], panel = 4, ylabel = 'KD', color = 'yellow'),
               mpf.make_addplot(stock_df["d"], panel = 4,  color = 'blue')
             ]
-    mpf.plot(stock_df,type='candle', style = s, mav=(5,10,20), addplot = index,volume=True)
+
+    ##畫圖
+    mpf.plot(stock_df,type='candle',  title = stock_num ,style = s, mav=(5,10,20), addplot = index,volume=True)  ## mav = MA
 else:
     print("wrong number")
