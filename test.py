587f8f1af55bc0bfe7f1ed6351822c1d0cbe2c0b
Harvey <56714146+yhh19910@users.noreply.github.com>
2022-06-04 23:40:51 +0800

    info(30%)

--- test.py
+++ test.py
@@ -105,10 +105,9 @@ def go():
     ##stock_num = str(input("輸入股票代碼:"))
     ##stock_num = '2330'
     skillpoint = [0,0,0,0] ##壓力 布林 均線 MACD
-
- 
     global canvas
     #canvas = None 
+    canvas.delete("info")
     stock_num = entry0.get()
 
     start = datetime.datetime(2021,9,24)
@@ -139,7 +138,7 @@ def go():
         today = datetime.datetime.today() - datetime.timedelta(days=0) ##2022-06-04
         # last_day =  today.strftime('%Y-%m-%d')
         last_day = stock_df.index.tolist()[-1]
-        print(type(stock_df.at[last_day,"Open"]))
+        #print(type(stock_df.at[last_day,"Open"]))
         ###MACDd
         stock_df["MACD"],stock_df["MACDsignal"],stock_df["MACDhist"] = talib.MACD(np.array(close),fastperiod = 6,slowperiod = 12,signalperiod = 9)
         
@@ -209,59 +208,69 @@ def go():
         #stock_df.to_csv('./SVM/'+stock_num+'.csv')
 
         #顯示資訊
-        canvas.create_text(
-        726.5, 135.0,
-        text = "2330",
-        fill = "#fd7014",
-        font = ("None", int(35.0)))
+        # canvas.create_text(
+        # 726.5, 135.0,
+        # text = "2330",
+        # fill = "#fd7014",
+        # font = ("None", int(35.0)))
 
         canvas.create_text(
         1358.5, 226.0,
         text = "score",
         fill = "#eeeeee",
-        font = ("None", int(35.0)))
+        font = ("None", int(35.0)),
+        tag="info")
 
         canvas.create_text(
         510.0, 789.5,
-        text = "start	\n547.00\nhigh	\n547.00\nlow	\n535.00",
+        text = "Open \n"+str(round(stock_df.at[last_day,"Open"],0))+"\nClose \n"+str(round(stock_df.at[last_day,"Close"],0))+"\nVolume \n"+str(round(stock_df.at[last_day,"Volume"],0)),
         fill = "#000000",
-        font = ("None", int(26.0)))
+        font = ("None", int(26.0)),
+        tag="info")
+
+        #stock_df.at[last_day,"Open"]
+        #print(stock_df.info())
 
         canvas.create_text(
         721.0, 789.5,
-        text = "value\n13.95\nP/E ratio	\n21.15\nyield\n2.04%",
+        text = "High \n"+str(round(stock_df.at[last_day,"High"],0))+"\nLow \n"+str(round(stock_df.at[last_day,"Low"],0)),
         fill = "#000000",
-        font = ("None", int(26.0)))
+        font = ("None", int(26.0)),
+        tag="info")
 
-        canvas.create_text(
-        934.0, 789.5,
-        text = "CDP	\nB\n52 wk high\n688.00\n52 wk low	\n518.00",
-        fill = "#000000",
-        font = ("None", int(26.0)))
+        # canvas.create_text(
+        # 934.0, 789.5,
+        # text = "CDP	\nB\n52 wk high\n688.00\n52 wk low	\n518.00",
+        # fill = "#000000",
+        # font = ("None", int(26.0)))
 
         canvas.create_text(
         1265.0, 355.0,
         text = "ma",
         fill = "#000000",
-        font = ("None", int(35.0)))
+        font = ("None", int(35.0)),
+        tag="info")
 
         canvas.create_text(
         1429.0, 454.0,
         text = "8",
         fill = "#000000",
-        font = ("None", int(100.0)))
+        font = ("None", int(100.0)),
+        tag="info")
 
         canvas.create_text(
         1497.0, 503.0,
         text = "/10",
         fill = "#000000",
-        font = ("None", int(30.0)))
+        font = ("None", int(30.0)),
+        tag="info")
 
         canvas.create_text(
         1263.5, 491.5,
         text = "ma Long \nma short\n3 day gold\n3 dat dead",
         fill = "#000000",
-        font = ("None", int(25.0)))
+        font = ("None", int(25.0)),
+        tag="info")
 
     else:
         print("wrong number")
@@ -363,62 +372,6 @@ b3.place(
     x = 1057, y = 18,
     width = 64,
     height = 47)
-
-#文字顯示
-
-canvas.create_text(
-    726.5, 135.0,
-    text = "stock",
-    fill = "#fd7014",
-    font = ("None", int(35.0)))
-
-canvas.create_text(
-    1358.5, 226.0,
-    text = "score",
-    fill = "#eeeeee",
-    font = ("None", int(35.0)))
-
-canvas.create_text(
-    510.0, 789.5,
-    text = "start	\n547.00\nhigh	\n547.00\nlow	\n535.00",
-    fill = "#000000",
-    font = ("None", int(26.0)))
-
-canvas.create_text(
-    721.0, 789.5,
-    text = "value\n13.95\nP/E ratio	\n21.15\nyield\n2.04%",
-    fill = "#000000",
-    font = ("None", int(26.0)))
-
-canvas.create_text(
-    934.0, 789.5,
-    text = "CDP	\nB\n52 wk high\n688.00\n52 wk low	\n518.00",
-    fill = "#000000",
-    font = ("None", int(26.0)))
-
-canvas.create_text(
-    1265.0, 355.0,
-    text = "ma",
-    fill = "#000000",
-    font = ("None", int(35.0)))
-
-canvas.create_text(
-    1429.0, 454.0,
-    text = "8",
-    fill = "#000000",
-    font = ("None", int(100.0)))
-
-canvas.create_text(
-    1497.0, 503.0,
-    text = "/10",
-    fill = "#000000",
-    font = ("None", int(30.0)))
-
-canvas.create_text(
-    1263.5, 491.5,
-    text = "ma Long \nma short\n3 day gold\n3 dat dead",
-    fill = "#000000",
-    font = ("None", int(25.0)))
 
 #顯示走勢圖
 # trend_img = PhotoImage(file = f"output.png")
